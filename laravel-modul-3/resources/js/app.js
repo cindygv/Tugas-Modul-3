7ecca4a0ef005c4edece92fc6f331572e0d800cc
cindygv <111652246+cindygv@users.noreply.github.com>
2023-05-10 22:55:43 +0700

    Add files via upload

---
+++ laravel-modul-3/resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import.meta.glob(["../images/bs-icons.png"]);
+import.meta.glob(["../images/bs-themes.png"]);
+import.meta.glob(["../images/logo-black.svg"]);
+import.meta.glob(["../images/logo-white.svg"]);
+import.meta.glob(["../images/main.svg"]);
